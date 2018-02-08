a2dd95f567af33986f62f4c2345ebe56eaddda90
Poladashili <ge0rge9.git@gmail.com>
2018-02-08 15:04:44 +0200

    upload headers btns

---
+++ src/js/partials/data.js
@@ -0,0 +1,11 @@
+const products = {
+    phones: {
+        apple: [
+            {
+                name: 'Iphone X',
+                price: 1500,
+                amount: 33
+            }
+        ]
+    }
+};
